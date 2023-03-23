a652c96f6474759c12a7a10b9af2029def6cfeac
bharah08 <111727649+bharah08@users.noreply.github.com>
2023-03-23 17:28:26 +0530

    Delete var directory

--- var/build.groovy
+++
@@ -1,4 +0,0 @@
-def call(){
-  echo "build the package using with maven"
-  sh 'mvn clean package'
-}
--- var/upload.groovy
+++
@@ -1,4 +0,0 @@
-def call(){
-  echo "upload artfactory to s3 bucket"
-  sh 'aws cp s3${WORKSPACE}/target/*.war s3://arts3up
-}
